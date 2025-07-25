463ae795da4fc95d8dba6920d82d181ffc777229
netopsys <netopsys@users.noreply.github.com>
2025-07-25 15:43:17 +0200

    feat: add linter github action

--- tools/bump-version.sh
+++ tools/bump-version.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 # Bump script version: major, minor, or patch
+# feat — new feature
+# fix — bug fix
+# docs — documentation only
+# style — formatting, whitespace, no functional changes
+# refactor — code refactoring without functional changes
+# perf — performance improvement
+# test — adding or fixing tests
+# chore — miscellaneous tasks, maintenance
 
 set -e
 
+MSG_COMMIT="feat: bump version to "
 VERSION_FILE="VERSION"
+README_FILE="README.md"
+
 
 if [ ! -f "$VERSION_FILE" ]; then
   echo "0.0.0" > "$VERSION_FILE"
@@ -34,11 +45,14 @@ esac
 
 new_version="${major}.${minor}.${patch}"
 echo "$new_version" > "$VERSION_FILE"
-
 echo "✅ New version: $new_version"
+
+version=$(cat "$VERSION_FILE")
+sed -i -E "s|(version-)[0-9]+\.[0-9]+\.[0-9]+(-blue.svg)|\1${version}\2|g" "$README_FILE"
+echo "README.md updated to version $version"
 
 # Git: create Release tag and push
 git add .
-git commit -am "chore: bump version to $new_version"
+git commit -am "$MSG_COMMIT $new_version"
 git tag -s "v$new_version" -m "Release v$new_version"
 git push origin v$new_version
